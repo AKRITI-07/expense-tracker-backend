6b38f8bb72244063fc9be0038e62c70b136428ab
AKRITI-07 <159432992+AKRITI-07@users.noreply.github.com>
2025-07-25 02:49:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ app.use(bodyParser.json());
 
 // MySQL connection
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',        // Change if needed
-  password: 'btsvlove',        // Your MySQL password here
-  database: 'expense_tracker'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
-
 db.connect(err => {
   if (err) throw err;
   console.log("MySQL Connected...");
